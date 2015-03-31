241a8aced3aeec79137a1c22e1ca534618d133cb
corneil <corneil.duplessis@gmail.com>
2015-03-31 22:56:04 +0200

    added more version for testing

--- installGroovyVersions.sh
+++ installGroovyVersions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 source ~/.gvm/bin/gvm-init.sh
-for version in "2.0.6" "2.1.9"  "2.2.2"  "2.3.9"  "2.4.3"
+for version in "2.0.6" "2.1.9" "2.2.1" "2.2.2" "2.3.0" "2.3.1" "2.4.3"
 do
     gvm install groovy "$version"
 done
--- loadDeps.sh
+++ loadDeps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 source ~/.gvm/bin/gvm-init.sh
-for version in "2.0.6" "2.1.9"  "2.2.2"  "2.3.9"  "2.4.3"
+for version in "2.0.6" "2.1.9" "2.2.1" "2.2.2" "2.3.0" "2.3.1" "2.4.3"
 do
     gvm use groovy "$version"
     groovyVersion=$(groovy -version)
@@ -9,35 +9,17 @@ do
         echo "$groovyVersion"
         rm -rf grape
         mkdir grape
-        ENS_OPTIONS="-Dgrape.root=./grape -Dgrape.config=./grapeConfig.xml -Dgroovy.grape.report.downloads=true -Divy.message.logger.level=4"
-        groovy $ENS_OPTIONS ensureDependencies.groovy > output-$version.log
-        if [[ $? != 0 ]]
-        then
-            echo "Error #1 with version $version"
-        else
-            echo "Success #1 with version $version"
-        fi
-        groovy $ENS_OPTIONS ensureDependencies2.groovy >> output-$version.log
-        if [[ $? != 0 ]]
-        then
-            echo "Error #2 with version $version"
-        else
-            echo "Success #2 with version $version"
-        fi
-        groovy $ENS_OPTIONS ensureDependencies3.groovy >> output-$version.log
-        if [[ $? != 0 ]]
-        then
-            echo "Error #3 with version $version"
-        else
-            echo "Success #3 with version $version"
-        fi
-        groovy $ENS_OPTIONS ensureDependencies4.groovy >> output-$version.log
-        if [[ $? != 0 ]]
-        then
-            echo "Error #4 with version $version"
-        else
-            echo "Success #4 with version $version"
-        fi
+        ENS_OPTIONS="-Dgrape.root=./grape -Dgrape.config=./grapeConfig.xml -Dgroovy.grape.report.downloads=false -Divy.message.logger.level=4"
+        for script in "ensureDependencies.groovy" "ensureDependencies2.groovy" "ensureDependencies3.groovy" "ensureDependencies4.groovy"
+        do
+            groovy $ENS_OPTIONS $script >> output-$version.log
+            if [[ $? != 0 ]]
+            then
+                echo "Error in $script with version $version"
+            else
+                echo "Success in $script with version $version"
+            fi
+        done
     else
         echo "Expected $version in $groovyVersion"
     fi
